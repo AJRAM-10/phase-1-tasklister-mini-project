6d962e6f07245714a9a01091db7626b4c7d4313c
Anthony Ramirez <tonyjo.ramirez@gmail.com>
2023-04-12 16:27:47 -0400

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(document.querySelector("#new-task-description").value)
+  })
 });
+
+function buildToDo(todo) {
+  let li = document.createElement('li')
+  li.textContent = `${todo}`
+  console.log(li)
+  document.querySelector('#tasks').appendChild(li)
+}
